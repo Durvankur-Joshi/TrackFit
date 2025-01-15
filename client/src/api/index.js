b055d4c00f0fce686456bed4f87f1d5d514a8b72
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-01-15 20:14:50 +0530

    Modified client/src/api/index.js for API updates

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+
+
 const API = axios.create({
   baseURL: "http://localhost:8080/api/",
 });
+
+
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
 export const UserSignIn = async (data) => API.post("/user/signin", data);
